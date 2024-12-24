058c989e7e3b6713833709d290dd2723268fde29
sahib775 <ghulammurtaza7751618@gmail.com>
2024-12-24 19:14:32 +0000

    String Methods

---
+++ StrongFunctions.js
@@ -0,0 +1,15 @@
+/* Memory Types
+2 types:
+1:Stack  , 2:Heap
+Stach:used in primitive types
+Heap:used in non primitive type
+*/
+// Stack take the copy 
+// Heap take the reference
+
+// String Interpulation
+const name = "Ghulam Murtaza";
+const age = 24;
+console.log(`hello my name is ${name} length is ${name.length} and my age is ${age}`);
+console.log(name.charAt(3));
+console.log(name.indexOf('m'));
